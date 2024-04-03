71bb8c5266169c163c590718bf2753b6a2bfb195
crazyoptimist <hey@crazyoptimist.net>
2024-04-03 10:59:15 -0500

    refactor: organize into lib, for integration tests

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,6 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
-
-#[get("/health_check")]
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
+use zero2prod::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(health_check))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    run().await
 }
