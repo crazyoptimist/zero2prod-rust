212b206c7e49f5227e7f148d8a6bcce1906d5b6d
crazyoptimist <hey@crazyoptimist.net>
2024-04-04 03:20:25 -0500

    test: write failing ones for subscribe endpoint

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -6,13 +6,15 @@ use reqwest::Client;
 
 use zero2prod::run;
 
-// Spawn the server as a background task, because it should not block the test
+/// Spin up an instance of our application
+/// and returns its address (i.e. http://localhost:XXXX)
 fn spawn_app() -> String {
     // Port 0 is a wildcard port that tells the system to find an available port
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
     let server = run(listener).expect("Failed to listen");
 
+    // Run it as a background task, because it should not block the test
     let _ = spawn(server);
 
     format!("http://127.0.0.1:{}", port)
@@ -20,16 +22,60 @@ fn spawn_app() -> String {
 
 #[actix_web::test]
 async fn health_check_works() {
-    let address = spawn_app();
-
+    let app_address = spawn_app();
     let client = Client::new();
 
     let response = client
-        .get(&format!("{}/health_check", address))
+        .get(&format!("{}/health_check", app_address))
         .send()
         .await
         .expect("Failed to execute requeset.");
 
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
+}
+
+#[actix_web::test]
+async fn subscribe_returns_200_for_valid_form_data() {
+    let app_address = spawn_app();
+    let client = Client::new();
+
+    let body = "name=le%20guin&email=le_guin%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[actix_web::test]
+async fn subscribe_returns_400_when_data_is_missing() {
+    let app_address = spawn_app();
+    let client = Client::new();
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload was {}",
+            error_message
+        );
+    }
 }
