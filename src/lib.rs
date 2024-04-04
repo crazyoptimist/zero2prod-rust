eb1a0b8c9cd4510ca6a581e93d9c5455a813d10b
crazyoptimist <hey@crazyoptimist.net>
2024-04-04 03:43:43 -0500

    feat: add minimal subscriptions endpoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,24 @@
 use std::net::TcpListener;
 
-use actix_web::{dev::Server, get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{dev::Server, get, web, App, HttpResponse, HttpServer, Responder};
 
 #[get("/health_check")]
 async fn health_check() -> impl Responder {
     HttpResponse::Ok().finish()
 }
 
+async fn subscribe() -> impl Responder {
+    HttpResponse::Ok().finish()
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().service(health_check))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .service(health_check)
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
 
     Ok(server)
 }
