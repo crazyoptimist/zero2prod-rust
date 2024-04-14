3d16c37ceab4fb04a722a9ad44089e31ab5df104
crazyoptimist <hey@crazyoptimist.net>
2024-04-14 15:05:38 -0500

    feat: replace Logger middleware with tracing stuff

--- src/startup.rs
+++ src/startup.rs
@@ -1,14 +1,15 @@
 use crate::routes;
-use actix_web::{dev::Server, middleware::Logger, web, App, HttpServer};
+use actix_web::{dev::Server, web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
 
 pub fn run(listener: TcpListener, connection_pool: PgPool) -> Result<Server, std::io::Error> {
     let connection_pool = web::Data::new(connection_pool);
 
     let server = HttpServer::new(move || {
         App::new()
-            .wrap(Logger::default())
+            .wrap(TracingLogger::default())
             .service(routes::health_check)
             .route("/subscriptions", web::post().to(routes::subscribe))
             .app_data(connection_pool.clone())
