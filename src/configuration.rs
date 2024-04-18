c5efa9354fbc1f2fe99eccd5f2a20d55c627a5ee
crazyoptimist <hey@crazyoptimist.net>
2024-04-18 14:57:30 -0500

    refactor: config per environment, db lazy connect

--- src/configuration.rs
+++ src/configuration.rs
@@ -4,7 +4,7 @@ use secrecy::{ExposeSecret, Secret};
 #[derive(serde::Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
-    pub application_port: u16,
+    pub application: ApplicationSettings,
 }
 
 #[derive(serde::Deserialize)]
@@ -16,13 +16,61 @@ pub struct DatabaseSettings {
     pub database_name: String,
 }
 
+#[derive(serde::Deserialize)]
+pub struct ApplicationSettings {
+    pub port: u16,
+    pub host: String,
+}
+
 impl Settings {
     pub fn new() -> Result<Self, ConfigError> {
+        let base_path = std::env::current_dir().expect("Failed to determine the current directory");
+        let configuration_directory = base_path.join("configuration");
+
+        let environment: Environment = std::env::var("APP_ENVIRONMENT")
+            .unwrap_or_else(|_| "local".into())
+            .try_into()
+            .expect("Failed to parse APP_ENVIRONMENT");
+        let environment_filename = format!("{}.yaml", environment.as_str());
+
         let settings = Config::builder()
-            .add_source(File::with_name("configuration"))
+            .add_source(File::from(configuration_directory.join("base.yaml")))
+            .add_source(File::from(
+                configuration_directory.join(environment_filename),
+            ))
             .build()?;
 
         settings.try_deserialize()
+    }
+}
+
+/// Possible runtime environments for the application
+pub enum Environment {
+    Local,
+    Production,
+}
+
+impl Environment {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Environment::Local => "local",
+            Environment::Production => "production",
+        }
+    }
+}
+
+impl TryFrom<String> for Environment {
+    type Error = String;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        match value.to_lowercase().as_str() {
+            "local" => Ok(Self::Local),
+            "production" => Ok(Self::Production),
+            other => Err(format!(
+                "{} is not a supported environment. Use either `local` or `production`.",
+                other
+            )),
+        }
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,13 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = Settings::new().expect("Failed to read configuration.");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
 
     let connection_pool =
-        PgPool::connect(configuration.database.connection_string().expose_secret())
-            .await
+        PgPool::connect_lazy(configuration.database.connection_string().expose_secret())
             .expect("Failed to connect to Postgres.");
 
     let listener = TcpListener::bind(address).expect("Failed to bind address.");
